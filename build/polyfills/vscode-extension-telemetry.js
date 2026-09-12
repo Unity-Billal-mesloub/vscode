3e7875936563d4dada53edf5be6e791686eea83e
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-12 22:51:03 +0100

    Create vscode-extension-telemetry.js

---
+++ build/polyfills/vscode-extension-telemetry.js
@@ -0,0 +1,21 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+
+let TelemetryReporter = (function () {
+	function TelemetryReporter(extensionId, extensionVersion, key) {
+	}
+	TelemetryReporter.prototype.updateUserOptIn = function (key) {
+	};
+	TelemetryReporter.prototype.createAppInsightsClient = function (key) {
+	};
+	TelemetryReporter.prototype.getCommonProperties = function () {
+	};
+	TelemetryReporter.prototype.sendTelemetryEvent = function (eventName, properties, measurements) {
+	};
+	TelemetryReporter.prototype.dispose = function () {
+	};
+	TelemetryReporter.TELEMETRY_CONFIG_ID = 'telemetry';
+	TelemetryReporter.TELEMETRY_CONFIG_ENABLED_ID = 'enableTelemetry';
+	return TelemetryReporter;
+}());
+exports.default = TelemetryReporter;
