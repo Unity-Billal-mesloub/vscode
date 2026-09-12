3b359890ad41b5ae6ec5a98a85e08d86c3a63ed4
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-12 20:58:38 +0100

    Remove copyright comments from test file
    Removed copyright comments from the test file.

--- .eslint-plugin-local/tests/code-no-reader-after-await-test.ts
+++ .eslint-plugin-local/tests/code-no-reader-after-await-test.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
 
 // Test file to verify the code-no-reader-after-await ESLint rule works correctly
 
